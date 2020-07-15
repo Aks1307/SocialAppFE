873543c72ba2a72e1784ddf821f0291f19a07c7b
Aks1307 <Aks130799@gmail.com>
2020-07-15 14:10:42 +0530

    navbar

--- socialmedia/src/App.js
+++ socialmedia/src/App.js
@@ -1,28 +1,49 @@
-import React,{Component} from 'react';
-import './App.css';
-import { BrowserRouter as Router,Route,Switch } from 'react-router-dom'
-import home from './pages/home'
-import login from './pages/login'
-import signUp from './pages/signup'
-import Navbar from './componenets/navbar';
-class App extends Component{
-  render(){
-  return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <div className="container">
-        <Switch>
-          <Route exact path='/' component={home}/>
-          <Route exact path='/login' component={login}/>
-          <Route exact path='/signUp' component={signUp}/>
-        </Switch>
-          </div>   
-        
-      </Router>
-    
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+import home from "./pages/home";
+import login from "./pages/login";
+import signUp from "./pages/signup";
+import Navbar from "./componenets/navbar";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#212121",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <Route exact path="/login" component={login} />
+                <Route exact path="/signUp" component={signUp} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
--- socialmedia/src/componenets/navbar.js
+++ socialmedia/src/componenets/navbar.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react'
-
+import React, { Component } from "react";
+import Link from "react-router-dom/Link";
 // material
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
 export class navbar extends Component {
-    render() {
-        return (
-           <AppBar position = 'fixed'>
-               <Toolbar>
-                    <Button color="inherit">LogIn</Button>
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">SignUp</Button>
-               </Toolbar>
-           </AppBar>
-        )
-    }
+  render() {
+    return (
+      <AppBar position="fixed">
+        <Toolbar className="nav-container">
+          <Button color="inherit" component={Link} to="/login">
+            LogIn
+          </Button>
+          <Button color="inherit" component={Link} to="/">
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="signUp">
+            SignUp
+          </Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
 }
 
-export default navbar
+export default navbar;
